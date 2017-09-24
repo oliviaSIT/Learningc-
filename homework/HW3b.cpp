c89f7c1bb5c0a73f5304cb068557dd9301d0a7a3
oliviaSIT <olivia.lee8321@gmail.com>
2017-09-24 15:46:15 -0400

    rename

--- homework/HW3b.cpp
+++ homework/HW3b.cpp
@@ -1,99 +1,123 @@
+// author: Jiabin.Li
+
 #include <iostream>
-#include <string>
 #include <array>
 #include <fstream>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
+// size of the grid is 10*10
+const size_t ARRAY_SIZE = 10;
+
 class GameLife {
-public:
-	int generation;
+private:
+	int totalGeneration;
 	int frequency;
-	int L[10][10];
+	int L[ARRAY_SIZE + 2][ARRAY_SIZE + 2];
 
-	GameLife() {
-		generation = 0;
-		frequency = 0;
-		L[10][10] = {};
+	void readInitState(string filename) {
+
+        	string line;
+
+        	ifstream fs(filename);
+
+        	// first line contains 1. total generation and 2. frequency
+        	getline(fs, line);
+        	istringstream iss(line);
+        	iss >> totalGeneration;
+        	iss >> frequency;
+
+        	// following lines are the init state of the matrix
+        	for (size_t i = 1; i <= ARRAY_SIZE; i++) {
+            		getline(fs, line);
+            		for (size_t j = 1; j <= ARRAY_SIZE; j++) {
+                		if (line[j - 1] == '*')
+                    			L[i][j] = 1;
+                		else
+                    			L[i][j] = 0;
+            		}
+        	}
+		
+		// add the edge
+		for (size_t j = 0; j <= ARRAY_SIZE + 1; j++) {
+			L[0][j] = 0;
+			L[j][0] = 0;
+			L[ARRAY_SIZE + 1][j] = 0;
+			L[j][ARRAY_SIZE + 1] = 0;
+		}
+
+		
+        	fs.close();
+    	}
+
+public:
+	GameLife(string filename) {
+        	readInitState(filename);
+        }
+
+	void startPrint() { 	
+		for (int generation = 0; generation <= totalGeneration; generation++) {
+			if (generation % frequency == 0) {
+				cout << "Current generation is = " << generation << endl;
+				printLife();
+				cout << endl;
+			}
+			update();
+		}
+
 	}
 
-	void read(string s) {
-		ifstream fs(s);
-		fs >> generation >> frequency;
-	
-		for(int i = 0; i <= 9; i++) {
-			fs >> L[i][0] >> L[i][1] >> L[i][2] >> L[i][3] >> L[i][4] >> L[i][5] >> L[i][6] >> L[i][7] >> L[i][8] >> L[i][9] >> endl;
-                }		
-		fs.close();
-	}
+	void updateNeighbour(size_t i, size_t j, int (&neighbour)[ARRAY_SIZE + 2][ARRAY_SIZE + 2]) {
+                int temp;
+		for (size_t h = i - 1; h <= i + 1; h++) {
+                        for (size_t w = j - 1; w <= j + 1; w++) {
+                                if (h == i && w == j)
+                                        continue;
+                              
+                  		temp = neighbour[h][w] + 1;
+				neighbour[h][w] = temp;
+                        }
+                }
+        }
 
-//	void print() {
-//		for (int i = 0; i <= generation; i++) {
-//			if (i % frequency == 0) {
-//				cout << "generation = " << i;
-//				printIJ(i);
-//				cout << endl;
-//			}
-//			update(L);
-//		}
-//	}
-//
-//	void printIJ(int g) {
-//		for (int i = 0; i <= 9; i++) {
-//			for (int j = 0; j <= 9; j++) {
-//				if (L[i][j]) {
-//					cout << "*";
-//				} else {
-//					cout << " ";
-//				}
-//			}
-//			cout << endl;
-//		}
-//	}
-//
-//
-//	void update(int &L[10][10]){
-//	        for (int i = 0; i <= 9; i++) {
-//			for (int j = 0; j <= 9; j++) {
-//				 if (L[i][j] == 1)
-//                   			 L[i][j] = 10;
-//            		}
-//        	}	
-//
-//      	        for (int i = 0; i <= 9; i++) {
-//           		 for (int j = 0; j <= 9; j++) {
-//				updateNeighbors(i, j, L);
-//			        if (L[i][j] == 3 || L[i][j] == 12 || L[i][j] == 13)
-//					L[i][j] = 1;
-//			        else 
-//					L[i][j] = 0;
-//            		 }
-//        	}
-//		
-//        }
-//        
-//
-//	void updateNeibours(int i, int j, int &L[10][10]) {
-//                bool isLive = L[i][j] >= 10;
-//                for (int h = i - 1; h <= i + 1; h++) {
-//               	         if (h < 0 || h > 9)
-//                                continue;
-//                         for (int w = j - 1; w <= j + 1; w++) {
-//                                if (w < 0 || w > 9)
-//                                     continue;
-//
-//                                if (h == i && w == j)
-//                                     continue;
-//
-//                                if (isLive)
-//                                     ++L[h][w];
-//                          }
-//                }
-//	}
+	void update() {
+		int neighbour[ARRAY_SIZE + 2][ARRAY_SIZE + 2] = {};
+		for (size_t i = 1; i <= ARRAY_SIZE; i++) {
+                	for (size_t j = 1; j <= ARRAY_SIZE; j++) {
+				if (L[i][j] == 1) {
+                			updateNeighbour(i, j, neighbour);
+				}
+			}
+		}
+
+		for (size_t i = 1; i <= ARRAY_SIZE; i++) {
+                        for (size_t j = 1; j <= ARRAY_SIZE; j++) {
+				if ((L[i][j] == 1 && neighbour[i][j] == 2) || neighbour[i][j] == 3)
+                                	L[i][j] = 1;
+                		else
+                    			L[i][j] = 0;
+            		}
+        	}
+	}  
+
+	void printLife() const {
+		for (size_t i = 1; i <= ARRAY_SIZE; i++) {
+            		for (size_t j = 1; j <= ARRAY_SIZE; j++) {
+                		if (L[i][j] == 1) {
+					cout << "*";
+				} else {
+					cout << " ";
+				}
+            		}
+            		cout << endl;
+        	}
+	}
 };
 
 int main() {
-	GameLife solution;
-	solution.read(".dat");
+	GameLife sol("test.dat");
+        sol.startPrint();
 	return 0;
 }
