4b1a4a7959bb3dc3d74409a7666781e0be0bb223
oliviaSIT <olivia.lee8321@gmail.com>
2017-10-10 21:19:25 -0400

    HW4c output 255,255,255

--- homework/HW4c.cpp
+++ homework/HW4c.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstdint>
 #include <iomanip>
+#include <bitset>
 
 using namespace std;
 
@@ -48,11 +49,11 @@ public:
                 return rgba[r * cols + c];
         }
 	
-
+*/
 	uint32_t& operator()(uint32_t c, uint32_t r) {
                 return rgba[r * cols + c];
         }
-*/
+
 	
         ~Bitmap() {
             delete [] rgba;
@@ -60,7 +61,7 @@ public:
 
 	Bitmap& horizLine(uint32_t c1, uint32_t c2, uint32_t r, uint32_t x) {
 		for (uint32_t i = c1; i <= c2; i++) {
-			rgba[ r * cols + i] = x;
+			rgba[r * cols + i] = x;
 		}
 		return *this;
 	}
@@ -77,9 +78,17 @@ public:
 };
 
 ostream& operator<<(ostream& s, const Bitmap& a) {
+	bitset<32> rgb;
+	bitset<8> r, g, b;
 	for (uint32_t i = 0; i < a.rows; i++) {
                 for (uint32_t j = 0; j< a.cols; j++) {
-                                s << setw(4) << a.rgba[i * a.cols + j];
+			rgb = a.rgba[i * a.cols + j];
+			for (size_t k = 0; k < 8; k++) {
+		                b[k] = rgb[k];
+				g[k] = rgb[k + 8];
+				r[k] = rgb[k + 16];
+			}
+			s << setw(10) << b.to_ulong() << "," << g.to_ulong() << "," << r.to_ulong() << " ";
                 }
 		s << endl;		
 	}
@@ -93,4 +102,15 @@ int main() {
 	b2.horizLine(3, 15, 0, 0xFF00FF); // go from (3,0) to (15,0) writing color
 	b2.vertLine(0, 8, 0, 0x000100);// go from (0,0) to (0,8) writing color
 	cout << b2;
+	uint32_t y = b2(0,8);
+	cout << y << endl;
+
+/*	uint32_t x =16711935;
+	bitset<32> rgb(x);
+	bitset<8> r, g, b;
+	for (size_t i = 0; i < 8; i++) 
+		b[i] = rgb[i + 8]; 	
+	cout << b << endl;
+*/
+	return 0;
 }	
