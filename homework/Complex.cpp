b7a6c8fc5c410687f66c0b355ec10a5c5fa77577
oliviaSIT <olivia.lee8321@gmail.com>
2017-12-08 00:06:06 -0500

    refinte main of complex

--- homework/Complex.cpp
+++ homework/Complex.cpp
@@ -3,25 +3,18 @@
 
 using namespace std;
 
-int main() {
-    Complex<double> c(1.1, -2.1);
-//    cout << sizeof(c.abs()) << endl;
-    c.print();   
- 
-    Complex<double> x(c);
-    cout << x << endl;
-    x.print();    
+int main(){
+  Complex<float> a(1.0f, 1.5f);
+  Complex<double> b(1.0, 1.5);
+  Complex<long double> c(1.0L, 2.5L);
 
-    Complex<double> r = -c; 
-    cout << r << endl;
-    r.print();  
-  
-   // r = c;
-   // cout << r << endl;
-
-    Complex<double> d;
-    d = c + x;
-    d.print();    
+  Complex<double> d(1.5, 1.0);
+  Complex<double> e = b + d;
+  cout << e << '\n';
+  cout << a.abs() << '\n'; // compute the absolute value 
+  cout << b.abs() << '\n'; // compute the absolute value 
+  cout << c.abs() << '\n'; // compute the absolute value 
+   
 
     return 0;
 }
